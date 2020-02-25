a0dec225fa7dc5cb7d922db3d5a7bea268194a58
Christopher Lien <cnlien@gmail.com>
2020-02-25 16:41:33 -0500

    rendering lat and long to the dom with conditional rendering for errors and loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Container, Spinner } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  // Constructor loads immediately on load. Not required but if it's used you must call super(props);
+  constructor(props){
+    super(props);
+
+    this.state = {
+      lat: null,
+      long: null,
+      errorMessage: "",
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.table(position)
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        })
+      },
+      (err) => {
+        console.log(err);
+        this.setState({
+          errorMessage: err.message,
+        })
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return(
+        <Container>
+          <h3>Error: {this.state.errorMessage}</h3>
+        </Container>
+      )
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <Container>
+          <h3>Latitude: {this.state.lat}</h3>
+          <h3>Longitude: {this.state.long}</h3>
+        </Container>
+      )
+    } else {
+      return (
+        <Container>
+          <h3>Latitude: <Spinner color="dark" /></h3>
+          <h3>Longitude: <Spinner color="dark" /></h3>
+        </Container>
+      );
+    }
+  }
 }
+
+
 
 export default App;
---
+++ src/Components/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+    return(
+        <div>
+            <h4>Season Display</h4>
+        </div>
+    )
+}
+
+export default SeasonDisplay;
